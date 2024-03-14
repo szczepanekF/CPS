a51d40ca455518d7a189ae55469ab1d7678114f1
Jakub Kowalik <j.kowalik0928@gmail.com>
2024-03-14 19:27:01 +0100

    file operations in main

--- src/run/main.cpp
+++ src/run/main.cpp
@@ -1,133 +1,224 @@
-//// Dear ImGui: standalone example application for GLFW + OpenGL 3, using programmable pipeline
-//// (GLFW is a cross-platform general purpose library for handling windows, inputs, OpenGL/Vulkan/Metal graphics context creation, etc.)
-//
-//// Learn about Dear ImGui:
-//// - FAQ                  https://dearimgui.com/faq
-//// - Getting Started      https://dearimgui.com/getting-started
-//// - Documentation        https://dearimgui.com/docs (same as your local docs/ folder).
-//// - Introduction, links and more at the top of imgui.cpp
-//
-//
-//#include "imgui.h"
-//#include "imgui_impl_glfw.h"
-//#include "imgui_impl_opengl3.h"
-//#include <stdio.h>
+#include "imgui.h"
+#include <stdio.h>
+#include <fstream>
+#include "signals/SignalStrategy.h"
+#include "operations/SignalProcesor.h"
+#include "signals/SinusoidalSignal.h"
+#include "matplotlibcpp.h"
+#include "../bindings/imgui_impl_opengl3.h"
+#include "../bindings/imgui_impl_glfw.h"
+
 //#define GL_SILENCE_DEPRECATION
-//#if defined(IMGUI_IMPL_OPENGL_ES2)
-//#include <GLES2/gl2.h>
-//#endif
-//#include <GLFW/glfw3.h> // Will drag system OpenGL headers
-//
+#if defined(IMGUI_IMPL_OPENGL_ES2)
+#include <GLES2/gl2.h>
+#endif
+
+#include <GLFW/glfw3.h> // Will drag system OpenGL headers
+
 //// [Win32] Our example includes a copy of glfw3.lib pre-compiled with VS2010 to maximize ease of testing and compatibility with old VS compilers.
 //// To link with VS2010-era libraries, VS2015+ requires linking with legacy_stdio_definitions.lib, which we do using this pragma.
 //// Your own project should not be affected, as you are likely to link with a newer binary of GLFW that is adequate for your version of Visual Studio.
-//#if defined(_MSC_VER) && (_MSC_VER >= 1900) && !defined(IMGUI_DISABLE_WIN32_FUNCTIONS)
-//#pragma comment(lib, "legacy_stdio_definitions")
-//#endif
-//
+#if defined(_MSC_VER) && (_MSC_VER >= 1900) && !defined(IMGUI_DISABLE_WIN32_FUNCTIONS)
+#pragma comment(lib, "legacy_stdio_definitions")
+#endif
+
 //// This example can also compile and run with Emscripten! See 'Makefile.emscripten' for details.
-//#ifdef __EMSCRIPTEN__
-//#include "../libs/emscripten/emscripten_mainloop_stub.h"
-//#endif
-//
-//static void glfw_error_callback(int error, const char* description)
-//{
-//    fprintf(stderr, "GLFW Error %d: %s\n", error, description);
-//>>>>>>> Stashed changes
-//}
-#include "signals/SignalStrategy.h"
-#include "Calculator.h"
-#include "signals/SinusoidalSignal.h"
-#include "matplotlibcpp.h"
+#ifdef __EMSCRIPTEN__
+#include "../libs/emscripten/emscripten_mainloop_stub.h"
+#endif
+
+static void glfw_error_callback(int error, const char *description) {
+    fprintf(stderr, "GLFW Error %d: %s\n", error, description);
+}
+
+
+void
+saveVectorsToBinary(const std::vector<double> &xData, const std::vector<double> &yData, const std::string &filename) {
+    // Open a binary file for writing
+    std::ofstream outFile(filename, std::ios::out | std::ios::binary | std::ios::trunc);
+    if (!outFile.is_open()) {
+        std::cerr << "Error opening file for writing!" << std::endl;
+        return;
+    }
+
+    // Write the size of the vectors
+    size_t sizeX = xData.size();
+    size_t sizeY = yData.size();
+    outFile.write(reinterpret_cast<const char *>(&sizeX), sizeof(sizeX));
+    outFile.write(reinterpret_cast<const char *>(&sizeY), sizeof(sizeY));
+
+    // Write the data of the vectors
+    outFile.write(reinterpret_cast<const char *>(xData.data()), sizeX * sizeof(double));
+    outFile.write(reinterpret_cast<const char *>(yData.data()), sizeY * sizeof(double));
+
+    // Close the file
+    outFile.close();
+
+    std::cout << "Vectors saved to file successfully." << std::endl;
+}
+
+void readVectorsFromBinary(std::vector<double> &xData, std::vector<double> &yData, const std::string &filename) {
+    // Open a binary file for reading
+    std::ifstream inFile(filename, std::ios::in | std::ios::binary);
+    if (!inFile.is_open()) {
+        std::cerr << "Error opening file for reading!" << std::endl;
+        return;
+    }
+
+    // Read the size of the vectors
+    size_t sizeX, sizeY;
+    inFile.read(reinterpret_cast<char *>(&sizeX), sizeof(sizeX));
+    inFile.read(reinterpret_cast<char *>(&sizeY), sizeof(sizeY));
+
+    // Resize vectors to fit the read data
+    xData.resize(sizeX);
+    yData.resize(sizeY);
+
+    // Read the data of the vectors
+    inFile.read(reinterpret_cast<char *>(xData.data()), sizeX * sizeof(double));
+    inFile.read(reinterpret_cast<char *>(yData.data()), sizeY * sizeof(double));
+
+    // Close the file
+    inFile.close();
+
+    std::cout << "Vectors read from file successfully." << std::endl;
+}
+
+
+void convertDataVector(std::vector<double> doubleVector, float *tab) {
+    size_t size = doubleVector.size();
+    for (size_t i = 0; i < size; ++i) {
+        tab[i] = static_cast<float>(doubleVector[i]);
+    }
+}
 
 namespace plt = matplotlibcpp;
+
 // Main code
-int main()
-{
-//    glfwSetErrorCallback(glfw_error_callback);
-//    if (!glfwInit())
-//        return 1;
-//
-//    // Decide GL+GLSL versions
-//#if defined(IMGUI_IMPL_OPENGL_ES2)
-//    // GL ES 2.0 + GLSL 100
-//    const char* glsl_version = "#version 100";
-//    glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 2);
-//    glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 0);
-//    glfwWindowHint(GLFW_CLIENT_API, GLFW_OPENGL_ES_API);
-//#elif defined(__APPLE__)
-//    // GL 3.2 + GLSL 150
-//    const char* glsl_version = "#version 150";
-//    glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
-//    glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 2);
-//    glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);  // 3.2+ only
-//    glfwWindowHint(GLFW_OPENGL_FORWARD_COMPAT, GL_TRUE);            // Required on Mac
-//#else
-//    // GL 3.0 + GLSL 130
-//    const char* glsl_version = "#version 130";
-//    glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
-//    glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 0);
-//    //glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);  // 3.2+ only
-//    //glfwWindowHint(GLFW_OPENGL_FORWARD_COMPAT, GL_TRUE);            // 3.0+ only
-//#endif
-//
-//    // Create window with graphics context
-//    GLFWwindow* window = glfwCreateWindow(1280, 720, "Dear ImGui GLFW+OpenGL3 example", nullptr, nullptr);
-//    if (window == nullptr)
-//        return 1;
-//    glfwMakeContextCurrent(window);
-//    glfwSwapInterval(1); // Enable vsync
-//
-//    // Setup Dear ImGui context
-//    IMGUI_CHECKVERSION();
-//    ImGui::CreateContext();
-//    ImGuiIO& io = ImGui::GetIO(); (void)io;
-//    io.ConfigFlags |= ImGuiConfigFlags_NavEnableKeyboard;     // Enable Keyboard Controls
-//    io.ConfigFlags |= ImGuiConfigFlags_NavEnableGamepad;      // Enable Gamepad Controls
-//
-//    // Setup Dear ImGui style
-//    ImGui::StyleColorsDark();
-//    //ImGui::StyleColorsLight();
-//
-//    // Setup Platform/Renderer backends
-//    ImGui_ImplGlfw_InitForOpenGL(window, true);
-//#ifdef __EMSCRIPTEN__
-//    ImGui_ImplGlfw_InstallEmscriptenCanvasResizeCallback("#canvas");
-//#endif
-//    ImGui_ImplOpenGL3_Init(glsl_version);
-//
-//    // Load Fonts
-//    // - If no fonts are loaded, dear imgui will use the default font. You can also load multiple fonts and use ImGui::PushFont()/PopFont() to select them.
-//    // - AddFontFromFileTTF() will return the ImFont* so you can store it if you need to select the font among multiple.
-//    // - If the file cannot be loaded, the function will return a nullptr. Please handle those errors in your application (e.g. use an assertion, or display an error and quit).
-//    // - The fonts will be rasterized at a given size (w/ oversampling) and stored into a texture when calling ImFontAtlas::Build()/GetTexDataAsXXXX(), which ImGui_ImplXXXX_NewFrame below will call.
-//    // - Use '#define IMGUI_ENABLE_FREETYPE' in your imconfig file to use Freetype for higher quality font rendering.
-//    // - Read 'docs/FONTS.md' for more instructions and details.
-//    // - Remember that in C/C++ if you want to include a backslash \ in a string literal you need to write a double backslash \\ !
-//    // - Our Emscripten build process allows embedding fonts to be accessible at runtime from the "fonts/" folder. See Makefile.emscripten for details.
-//    //io.Fonts->AddFontDefault();
-//    //io.Fonts->AddFontFromFileTTF("c:\\Windows\\Fonts\\segoeui.ttf", 18.0f);
-//    //io.Fonts->AddFontFromFileTTF("../../misc/fonts/DroidSans.ttf", 16.0f);
-//    //io.Fonts->AddFontFromFileTTF("../../misc/fonts/Roboto-Medium.ttf", 16.0f);
-//    //io.Fonts->AddFontFromFileTTF("../../misc/fonts/Cousine-Regular.ttf", 15.0f);
-//    //ImFont* font = io.Fonts->AddFontFromFileTTF("c:\\Windows\\Fonts\\ArialUni.ttf", 18.0f, nullptr, io.Fonts->GetGlyphRangesJapanese());
-//    //IM_ASSERT(font != nullptr);
-//
-//    // Our state
-//    bool show_demo_window = true;
-//    bool show_another_window = false;
-//    ImVec4 clear_color = ImVec4(0.45f, 0.55f, 0.60f, 1.00f);
-//
+int main() {
+    Signal signalFromFIle;
+    readVectorsFromBinary(signalFromFIle.signalValues, signalFromFIle.timeValues, "signal_data.bin");
+    int size = signalFromFIle.signalValues.size();
+    float *signalValues = new float[size];
+    float *timeValues = new float[size];
+    std::vector<float> xData = {1.0f, 2.0f, 3.0f, 4.0f, 5.0f};
+    std::vector<float> yData = {2.0f, 3.0f, 1.5f, 4.0f, 2.5f};
+
+    convertDataVector(signalFromFIle.signalValues, signalValues);
+    convertDataVector(signalFromFIle.timeValues, timeValues);
+    glfwSetErrorCallback(glfw_error_callback);
+    if (!glfwInit())
+        return 1;
+
+    // Decide GL+GLSL versions
+#if defined(IMGUI_IMPL_OPENGL_ES2)
+    // GL ES 2.0 + GLSL 100
+    const char* glsl_version = "#version 100";
+    glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 2);
+    glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 0);
+    glfwWindowHint(GLFW_CLIENT_API, GLFW_OPENGL_ES_API);
+#elif defined(__APPLE__)
+    // GL 3.2 + GLSL 150
+    const char* glsl_version = "#version 150";
+    glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
+    glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 2);
+    glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);  // 3.2+ only
+    glfwWindowHint(GLFW_OPENGL_FORWARD_COMPAT, GL_TRUE);            // Required on Mac
+#else
+    // GL 3.0 + GLSL 130
+    const char *glsl_version = "#version 130";
+    glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
+    glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 0);
+    //glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);  // 3.2+ only
+    //glfwWindowHint(GLFW_OPENGL_FORWARD_COMPAT, GL_TRUE);            // 3.0+ only
+#endif
+
+    // Create window with graphics context
+    GLFWwindow *window = glfwCreateWindow(1280, 720, "CPS zad1", nullptr, nullptr);
+    if (window == nullptr)
+        return 1;
+    glfwMakeContextCurrent(window);
+    glfwSwapInterval(1); // Enable vsync
+
+    // Setup Dear ImGui context
+    IMGUI_CHECKVERSION();
+    ImGui::CreateContext();
+    ImGuiIO &io = ImGui::GetIO();
+    (void) io;
+    io.ConfigFlags |= ImGuiConfigFlags_NavEnableKeyboard;     // Enable Keyboard Controls
+    io.ConfigFlags |= ImGuiConfigFlags_NavEnableGamepad;      // Enable Gamepad Controls
+
+    // Setup Dear ImGui style
+    ImGui::StyleColorsDark();
+    //ImGui::StyleColorsLight();
+
+    // Setup Platform/Renderer backends
+    ImGui_ImplGlfw_InitForOpenGL(window, true);
+#ifdef __EMSCRIPTEN__
+    ImGui_ImplGlfw_InstallEmscriptenCanvasResizeCallback("#canvas");
+#endif
+    ImGui_ImplOpenGL3_Init(glsl_version);
+
+    // Our state
+    bool show_demo_window = false;
+    bool show_another_window = false;
+    double ampInput = 0.0;
+
+    ImVec4 clear_color = ImVec4(0.10f, 0.40f, 0.80f, 1.00f);
+
 //    // Main loop
-//#ifdef __EMSCRIPTEN__
-//    // For an Emscripten build we are disabling file-system access, so let's not attempt to do a fopen() of the imgui.ini file.
-//    // You may manually call LoadIniSettingsFromMemory() to load settings from your own storage.
-//    io.IniFilename = nullptr;
-//    EMSCRIPTEN_MAINLOOP_BEGIN
-//#else
-//    while (!glfwWindowShouldClose(window))
-//#endif
-//    {
+#ifdef __EMSCRIPTEN__
+    // For an Emscripten build we are disabling file-system access, so let's not attempt to do a fopen() of the imgui.ini file.
+    // You may manually call LoadIniSettingsFromMemory() to load settings from your own storage.
+    io.IniFilename = nullptr;
+    EMSCRIPTEN_MAINLOOP_BEGIN
+#else
+    while (!glfwWindowShouldClose(window))
+#endif
+    {
+        glfwPollEvents();
+        ImGui_ImplOpenGL3_NewFrame();
+        ImGui_ImplGlfw_NewFrame();
+        ImGui::NewFrame();
+        ImGui::SetNextWindowSize(ImVec2(1200, 700));
+        ImGui::Begin("CPS zadanie 1", NULL,
+                     ImGuiWindowFlags_NoResize | ImGuiWindowFlags_NoMove | ImGuiWindowFlags_NoCollapse);
+//        ImGui::PlotLines("xd", (float (*)(void *,
+//                                          int)) reinterpret_cast<const float *>(signalFromFIle.timeValues.data()), signalFromFIle.signalValues.data(), size);
+ImVec2 plotSize(400, 300);
+ImGui::PlotLines("Data Plot", signalValues, size);
+
+        ImVec2 pos = ImGui::GetCursorScreenPos();
+
+        ImGui::GetWindowDrawList()->AddText(pos, IM_COL32_WHITE, "X Axis", NULL);
+
+//            if (ImGui::BeginTabItem("Tab 1")) {
+//                ImGui::InputDouble("Set amplitude value", &ampInput, 0.01, 0.1, "%.2f", ImGuiInputTextFlags_CharsDecimal);
+//                ImGui::Text("Amplitude for signal = %.2f", ampInput);
+//                ImGui::EndTabItem();
+//            }
+//
+//            if (ImGui::BeginTabItem("Tab 2")) {
+//
+//
+//            }
+//
+//            ImGui::EndTabBar();
+//        }
+        ImGui::End();
+
+        // Rendering
+        ImGui::Render();
+        int display_w, display_h;
+        glfwGetFramebufferSize(window, &display_w, &display_h);
+        glViewport(0, 0, display_w, display_h);
+        glClearColor(clear_color.x * clear_color.w, clear_color.y * clear_color.w, clear_color.z * clear_color.w,
+                     clear_color.w);
+        glClear(GL_COLOR_BUFFER_BIT);
+        ImGui_ImplOpenGL3_RenderDrawData(ImGui::GetDrawData());
+
+        glfwSwapBuffers(window);
+    }
 //        // Poll and handle events (inputs, window resize, etc.)
 //        // You can read the io.WantCaptureMouse, io.WantCaptureKeyboard flags to tell if dear imgui wants to use your inputs.
 //        // - When io.WantCaptureMouse is true, do not dispatch mouse input data to your main application, or clear/overwrite your copy of the mouse data.
@@ -149,7 +240,7 @@ int main()
 //            static float f = 0.0f;
 //            static int counter = 0;
 //
-//            ImGui::Begin("Hello, world!");                          // Create a window called "Hello, world!" and append into it.
+//            ImGui::Begin("CPS zadanie 1");                          // Create a window called "Hello, world!" and append into it.
 //
 //            ImGui::Text("This is some useful text.");               // Display some text (you can use a format strings too)
 //            ImGui::Checkbox("Demo Window", &show_demo_window);      // Edit bools storing our window open/close state
@@ -199,19 +290,31 @@ int main()
 //
 //    glfwDestroyWindow(window);
 //    glfwTerminate();
+//
+//    SignalStrategy* sinusoidalSignal = new SinusoidalSignal(2,2, 6, 0.4);
+//    SignalStrategy* sinusoidalSignal1 = new SinusoidalSignal(1,2, 6, 0.1);
+//    std::vector<SignalStrategy*>signals;
+//    Signal signalData = sinusoidalSignal->getSignal();
+//    saveVectorsToBinary(signalData.signalValues, signalData.timeValues, "signal_data.bin");
 
-    SignalStrategy* sinusoidalSignal = new SinusoidalSignal(2,2, 6, 0.4);
-    SignalStrategy* sinusoidalSignal1 = new SinusoidalSignal(1,2, 6, 0.1);
-    std::vector<SignalStrategy*>signals;
-    Signal signalData = sinusoidalSignal->getSignal();
-    Signal signalData1 = sinusoidalSignal1->getSignal();
-    plt::plot<>(signalData.timeValues, signalData.signalValues);
-    plt::plot<>(signalData1.timeValues, signalData1.signalValues);
-    signals.push_back(sinusoidalSignal);
-    signals.push_back(sinusoidalSignal1);
-    Calculator* calculator = new Calculator(signals);
-    Signal data = calculator->getCalculatedSignal("add");
-    plt::plot<>(data.timeValues, data.signalValues);
+//    Signal signalData1 = sinusoidalSignal1->getSignal();
+//    plt::plot<>(signalFromFIle.timeValues, signalFromFIle.signalValues);
+//    Signal signalFromFIle;
+//    readVectorsFromBinary(signalFromFIle.signalValues, signalFromFIle.timeValues, "signal_data.bin");
+//    float* signalValues = new float[signalFromFIle.signalValues.size()];
+//    float* timeValues = new float[signalFromFIle.timeValues.size()];
+//    convertDataVector(signalFromFIle.signalValues, signalValues);
+//    convertDataVector(signalFromFIle.timeValues, timeValues);
+
+
+    delete[] signalValues;
+    delete[] timeValues;
+//    plt::plot<>(signalData1.timeValues, signalData1.signalValues);
+//    signals.push_back(sinusoidalSignal);
+//    signals.push_back(sinusoidalSignal1);
+//    Calculator* calculator = new Calculator(signals);
+//    Signal data = calculator->getCalculatedSignal("add");
+//    plt::plot<>(data.timeValues, data.signalValues);
     plt::show();
-    return 0;
+//    return 0;
 }
