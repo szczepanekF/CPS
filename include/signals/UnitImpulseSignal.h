00634a2136dff85502fd8c07c1c185e112f3eded
242544 Szczepanek Filip <242544@edu.p.lodz.pl>
2024-03-18 19:37:24 +0100

    Fixes

--- include/signals/UnitImpulseSignal.h
+++ include/signals/UnitImpulseSignal.h
@@ -4,9 +4,9 @@
 #include "DiscreteSignal.h"
 class UnitImpulseSignal : public DiscreteSignal {
 public:
-    UnitImpulseSignal(double amp, double time0, double dur, double freq, double jumpTime);
+    UnitImpulseSignal(double amp, double time0, double dur, double freq, int jumpTime);
 
     double calculateSignalAt(double time) override;
 private:
-    double jumpTime;
+    int jumpTime;
 };
--- src/signals/UnitImpulseSignal.cpp
+++ src/signals/UnitImpulseSignal.cpp
@@ -3,11 +3,13 @@
 #include <cmath>
 #include "signals/UnitImpulseSignal.h"
 
-UnitImpulseSignal::UnitImpulseSignal(double amp, double time0, double dur, double freq, double jumpTime)
+UnitImpulseSignal::UnitImpulseSignal(double amp, double time0, double dur, double freq, int jumpTime)
         : DiscreteSignal(amp, time0, dur, freq), jumpTime(jumpTime) {
 
 }
 
 double UnitImpulseSignal::calculateSignalAt(double time) {
-    return std::fabs(time - jumpTime) < 0.001  ? getAmplitude() : 0;
+    int currentJumpNumber = std::floor((time - getBeginTime()) * getFrequency());
+
+    return currentJumpNumber == (jumpTime - 1) ? getAmplitude() : 0;
 }
