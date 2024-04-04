66342eb2d2af05fa9105a82405e888d20f568315
242544 Szczepanek Filip <242544@edu.p.lodz.pl>
2024-04-04 23:43:38 +0200

    Fix SincBasedReconstruction

--- include/signals/baseSignals/Signal.h
+++ include/signals/baseSignals/Signal.h
@@ -1,24 +1,57 @@
 #pragma once
+
 #include "vector"
 
 class Signal {
 
 public:
-    std::vector<double> timeValues;
-    std::vector<double> signalValues;
 
-    Signal operator-(const Signal& s) const;
-    Signal operator+(const Signal& s) const;
 
-    Signal operator/(const Signal& s) const;
-    Signal operator*(const Signal& s) const;
+    Signal operator-(const Signal &s) const;
+
+    Signal operator+(const Signal &s) const;
+
+    Signal operator/(const Signal &s) const;
+
+    Signal operator*(const Signal &s) const;
+
     double mean() const;
+
     double absMean() const;
+
     double variance() const;
+
     double meanPower() const;
+
     double rms() const;
+
+    double meanSquaredError(Signal other) const;
+
+    double signalToNoiseRatio(Signal other) const;
+
+    double peakSignalToNoiseRatio(Signal other) const;
+
+    double maxDifference(Signal other) const;
+
+    void push_back(double value, double time);
 
     size_t size() const;
 
-    void convertToFloat(float* vals, float* time) const;
+    bool empty() const;
+
+    void resize(size_t newSize);
+
+    void setValues(const std::vector<double> &sigVals, const std::vector<double> &timeVals);
+
+    void convertToFloat(float *vals, float *time) const;
+
+private:
+    std::vector<double> timeValues;
+public:
+    const std::vector<double> &getTimeValues() const;
+
+    const std::vector<double> &getSignalValues() const;
+
+private:
+    std::vector<double> signalValues;
 };
--- src/signals/baseSignals/Signal.cpp
+++ src/signals/baseSignals/Signal.cpp
@@ -1,12 +1,13 @@
 #include "signals/baseSignals/Signal.h"
 #include <cmath>
 #include <iostream>
+#include <algorithm>
 
 Signal Signal::operator-(const Signal &s) const {
     Signal ret;
     ret.timeValues = this->timeValues;
     ret.signalValues = std::vector<double>(this->signalValues.size());
-    for(size_t i = 0; i < signalValues.size(); i++) {
+    for (size_t i = 0; i < signalValues.size(); i++) {
         ret.signalValues[i] = this->signalValues[i] - s.signalValues[i];
     }
     return ret;
@@ -17,7 +18,7 @@ Signal Signal::operator+(const Signal &s) const {
     Signal ret;
     ret.timeValues = this->timeValues;
     ret.signalValues = std::vector<double>(this->signalValues.size());
-    for(size_t i = 0; i < signalValues.size(); i++) {
+    for (size_t i = 0; i < signalValues.size(); i++) {
         ret.signalValues[i] = this->signalValues[i] + s.signalValues[i];
     }
     return ret;
@@ -27,7 +28,7 @@ Signal Signal::operator/(const Signal &s) const {
     Signal ret;
     ret.timeValues = this->timeValues;
     ret.signalValues = std::vector<double>(this->signalValues.size());
-    for(size_t i = 0; i < signalValues.size(); i++) {
+    for (size_t i = 0; i < signalValues.size(); i++) {
         if (s.signalValues[i] == 0) continue;
         ret.signalValues[i] = this->signalValues[i] / s.signalValues[i];
     }
@@ -39,7 +40,7 @@ Signal Signal::operator*(const Signal &s) const {
     Signal ret;
     ret.timeValues = this->timeValues;
     ret.signalValues = std::vector<double>(this->signalValues.size());
-    for(size_t i = 0; i < signalValues.size(); i++) {
+    for (size_t i = 0; i < signalValues.size(); i++) {
         ret.signalValues[i] = this->signalValues[i] * s.signalValues[i];
     }
     return ret;
@@ -47,35 +48,35 @@ Signal Signal::operator*(const Signal &s) const {
 
 double Signal::mean() const {
     double sum = 0;
-    for(auto val : signalValues) {
+    for (auto val: signalValues) {
         sum += val;
     }
-    return sum / signalValues.size();
+    return sum / size();
 }
 
 double Signal::absMean() const {
     double sum = 0;
-    for(auto val : signalValues) {
+    for (auto val: signalValues) {
         sum += std::abs(val);
     }
-    return sum / signalValues.size();
+    return sum / size();
 }
 
 double Signal::variance() const {
     double meanVal = mean();
     double sum = 0;
-    for(auto val : signalValues) {
+    for (auto val: signalValues) {
         sum += std::pow(val - meanVal, 2);
     }
-    return sum / signalValues.size();
+    return sum / size();
 }
 
 double Signal::meanPower() const {
     double sum = 0;
-    for(auto val : signalValues) {
+    for (auto val: signalValues) {
         sum += std::pow(val, 2);
     }
-    return sum / signalValues.size();
+    return sum / size();
 }
 
 double Signal::rms() const {
@@ -91,4 +92,78 @@ void Signal::convertToFloat(float *vals, float *time) const {
         vals[i] = signalValues[i];
         time[i] = timeValues[i];
     }
+}
+
+double Signal::meanSquaredError(Signal other) const {
+    if (other.size() != size()) {
+        throw std::logic_error("Signals do not match size");
+    }
+
+    double sum = 0;
+    for (size_t i = 0; i < size(); i++) {
+        sum += std::pow(signalValues[i] - other.signalValues[i], 2);
+    }
+    return sum / size();
+}
+
+double Signal::signalToNoiseRatio(Signal other) const {
+    if (other.size() != size()) {
+        throw std::logic_error("Signals do not match size");
+    }
+    double meter = 0;
+    double denominator = 0;
+    for (size_t i = 0; i < size(); i++) {
+        meter += std::pow(signalValues[i], 2);
+        denominator += std::pow(signalValues[i] - other.signalValues[i], 2);
+    }
+    return 10 * std::log10(meter / denominator);
+}
+
+double Signal::peakSignalToNoiseRatio(Signal other) const {
+    if (other.size() != size()) {
+        throw std::logic_error("Signals do not match size");
+    }
+    auto maxElement = std::ranges::max_element(signalValues);
+    double mse = meanSquaredError(other);
+    return 10 * std::log10(*maxElement / mse);
+}
+
+double Signal::maxDifference(Signal other) const {
+    if (other.size() != size()) {
+        throw std::logic_error("Signals do not match size");
+    }
+    double max = 0;
+    for (size_t i = 0; i < size(); i++) {
+        max = std::max(max, std::abs(signalValues[i] - other.signalValues[i]));
+    }
+    return max;
+}
+
+bool Signal::empty() const {
+    return size() == 0;
+}
+
+void Signal::push_back(double value, double time) {
+    signalValues.push_back(value);
+    timeValues.push_back(time);
+
+}
+
+void Signal::resize(size_t newSize) {
+    signalValues.resize(newSize);
+    timeValues.resize(newSize);
+
+}
+
+void Signal::setValues(const std::vector<double> &sigVals, const std::vector<double> &timeVals) {
+    signalValues = sigVals;
+    timeValues = timeVals;
+}
+
+const std::vector<double> &Signal::getTimeValues() const {
+    return timeValues;
+}
+
+const std::vector<double> &Signal::getSignalValues() const {
+    return signalValues;
 }
